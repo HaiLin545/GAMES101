ae412730419a3172e484f10f78bd18594e8c53ee
hailin <hailin545@qq.com>
2024-05-23 15:44:41 +0800

    配置vcpkg+clion开发环境

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -6,21 +6,19 @@
 
 constexpr double MY_PI = 3.1415926;
 
-Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
-{
+Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos) {
     Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
 
     Eigen::Matrix4f translate;
     translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
-        -eye_pos[2], 0, 0, 0, 1;
+            -eye_pos[2], 0, 0, 0, 1;
 
     view = translate * view;
 
     return view;
 }
 
-Eigen::Matrix4f get_model_matrix(float rotation_angle, Eigen::Vector3f n = { 0.0f, 0.0f, 1.0f })
-{
+Eigen::Matrix4f get_model_matrix(float rotation_angle, Eigen::Vector3f n = {0.0f, 0.0f, 1.0f}) {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
 
     // TODO: Implement this function
@@ -35,8 +33,8 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle, Eigen::Vector3f n = { 0.0
     Eigen::Matrix3f N;
 
     N << 0, -n[2], n[1],
-        n[2], 0, -n[0],
-        -n[1], n[0], 0;
+            n[2], 0, -n[0],
+            -n[1], n[0], 0;
     N = cosa * I + (1.0 - cosa) * (n * n.transpose()) + sina * N;
 
     M.block(0, 0, 2, 2) = N.block(0, 0, 2, 2);
@@ -48,8 +46,7 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle, Eigen::Vector3f n = { 0.0
 }
 
 Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
-    float zNear, float zFar)
-{
+                                      float zNear, float zFar) {
     // Students will implement this function
 
     Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
@@ -67,24 +64,23 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
 
     Eigen::Matrix4f M_persp;
     M_persp << n, 0, 0, 0,
-        0, n, 0, 0,
-        0, 0, n + f, -n * f,
-        0, 0, 1, 0;
+            0, n, 0, 0,
+            0, 0, n + f, -n * f,
+            0, 0, 1, 0;
 
-    Eigen::Matrix4f  M_ortho_s;
+    Eigen::Matrix4f M_ortho_s;
     M_ortho_s << 2.0 / (r - l), 0, 0, 0,
-        0, 2.0 / (t - b), 0, 0,
-        0, 0, 2.0 / (n - f), 0,
-        0, 0, 0, 1;
+            0, 2.0 / (t - b), 0, 0,
+            0, 0, 2.0 / (n - f), 0,
+            0, 0, 0, 1;
 
     projection = M_ortho_s * M_persp * projection;
 
     return projection;
 }
 
-int main(int argc, const char** argv)
-{
-    float angle_z = 0, angle_x;
+int main(int argc, const char **argv) {
+    float angle_z = 0, angle_x = 0;
     bool is_axis_z = true, is_asis_x = false;
     bool command_line = false;
     std::string filename = "output.png";
@@ -99,14 +95,16 @@ int main(int argc, const char** argv)
 
     rst::rasterizer r(700, 700);
 
-    Eigen::Vector3f eye_pos = { 0, 0, 5 };
-    Eigen::Vector3f z = { 0, 0, 1.0 };
-    Eigen::Vector3f x = { 1.0, 0, 0 };
-    Eigen::Vector3f y = { 0, 1.0, 0 };
+    Eigen::Vector3f eye_pos = {0, 0, 5};
+    Eigen::Vector3f z = {0, 0, 1.0};
+    Eigen::Vector3f x = {1.0, 0, 0};
+    Eigen::Vector3f y = {0, 1.0, 0};
 
-    std::vector<Eigen::Vector3f> pos{ {2, 0, -2}, {0, 2, -2}, {-2, 0, -2} };
+    std::vector<Eigen::Vector3f> pos{{2,  0, -2},
+                                     {0,  2, -2},
+                                     {-2, 0, -2}};
 
-    std::vector<Eigen::Vector3i> ind{ {0, 1, 2} };
+    std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
 
     auto pos_id = r.load_positions(pos);
     auto ind_id = r.load_indices(ind);
@@ -147,29 +145,29 @@ int main(int argc, const char** argv)
         cv::imshow("image", image);
         key = cv::waitKey(10);
 
-        std::cout << "frame count: " << frame_count++ << '\n';
-
         switch (key) {
-        case 'a':
-            is_axis_z = true;
-            rotate_axis = z;
-            angle_z += 10;
-            break;
-        case 'd':
-            is_axis_z = true;
-            rotate_axis = z;
-            angle_z -= 10;
-            break;
-        case 'w':
-            is_axis_z = false;
-            rotate_axis = x;
-            angle_x += 10;
-            break;
-        case 's':
-            is_axis_z = false;
-            rotate_axis = x;
-            angle_x -= 10;
-            break;
+            case 'a':
+                is_axis_z = true;
+                rotate_axis = z;
+                angle_z += 10;
+                break;
+            case 'd':
+                is_axis_z = true;
+                rotate_axis = z;
+                angle_z -= 10;
+                break;
+            case 'w':
+                is_axis_z = false;
+                rotate_axis = x;
+                angle_x += 10;
+                break;
+            case 's':
+                is_axis_z = false;
+                rotate_axis = x;
+                angle_x -= 10;
+                break;
+            default:
+                break;
         }
 
     }
--- work0/main.cpp
+++ work0/main.cpp
@@ -5,17 +5,19 @@
 
 using namespace std;
 
-const double PI = cos(-1);
+const float PI = cos(-1.f);
 
 int main() {
 
     Eigen::Vector3f P(2, 1, 1);
     Eigen::Matrix3f M;
 
-    double a = PI / 4;
-    double tx = 1, ty = 2;
+    float a = PI / 4;
+    float tx = 1, ty = 2;
 
-    M << cos(a), -sin(a), tx, sin(a), cos(a), ty, 0, 0, 1;
+    M << cos(a), -sin(a), \
+         tx, sin(a), cos(a), \
+         ty, 0, 0, 1;
 
     cout << "P: " << endl;
     cout << P << "\n\n";
@@ -34,38 +36,42 @@ int main() {
     std::cout << std::sin(30.0 / 180.0 * acos(-1)) << std::endl;
 
     // Example of vector
-    // std::cout << "Example of vector \n";
-    // // vector definition
-    // Eigen::Vector3f v(1.0f, 2.0f, 3.0f);
-    // Eigen::Vector3f w(1.0f, 0.0f, 0.0f);
-    // // vector output
-    // std::cout << "Example of output \n";
-    // std::cout << v << std::endl;
-    // // vector add
-    // std::cout << "Example of add \n";
-    // std::cout << v + w << std::endl;
-    // // vector scalar multiply
-    // std::cout << "Example of scalar multiply \n";
-    // std::cout << v * 3.0f << std::endl;
-    // std::cout << 2.0f * v << std::endl;
+    std::cout << "Example of vector \n";
+    // vector definition
+    Eigen::Vector3f v(1.0f, 2.0f, 3.0f);
+    Eigen::Vector3f w(1.0f, 0.0f, 0.0f);
+    // vector output
+    std::cout << "Example of output \n";
+    std::cout << v << std::endl;
+    // vector add
+    std::cout << "Example of add \n";
+    std::cout << v + w << std::endl;
+    // vector scalar multiply
+    std::cout << "Example of scalar multiply \n";
+    std::cout << v * 3.0f << std::endl;
+    std::cout << 2.0f * v << std::endl;
 
-    // // Example of matrix
-    // std::cout << "Example of matrix \n";
-    // // matrix definition
-    // Eigen::Matrix3f i, j;
-    // i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
-    // j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
-    // // matrix output
-    // std::cout << "Example of output \n";
-    // std::cout << i << std::endl;
-    // // matrix add i + j
-    // // matrix scalar multiply i * 2.0
-    // // matrix multiply i * j
-    // // matrix multiply vector i * v
-    // cout << "Scalar multiply: \n";
-    // cout << i * 2.0 << endl;
-    // cout<< "Multiply matrix: \n";
-    // cout<< i * j <<endl;
+    // Example of matrix
+    std::cout << "Example of matrix \n";
+    // matrix definition
+    Eigen::Matrix3f i, j;
+    i << 1.0, 2.0, 3.0, \
+         4.0, 5.0, 6.0, \
+         7.0, 8.0, 9.0;
+    j << 2.0, 3.0, 1.0, \
+         4.0, 6.0, 5.0, \
+         9.0, 7.0, 8.0;
+    // matrix output
+    std::cout << "Example of output \n";
+    std::cout << i << std::endl;
+    // matrix add i + j
+    // matrix scalar multiply i * 2.0
+    // matrix multiply i * j
+    // matrix multiply vector i * v
+    cout << "Scalar multiply: \n";
+    cout << i * 2.0 << endl;
+    cout << "Multiply matrix: \n";
+    cout << i * j << endl;
 
 
     return 0;
